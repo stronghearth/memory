e82d5dac1585aa6305816cedd43ffbcc0306015b
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-05-02 13:52:35 -0400

    wrote checkForMatch

--- app.js
+++ app.js
@@ -21,15 +21,36 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: 'windflower',
             img: 'images/windflower.png'
+        },
+        {
+            name: 'cosmo',
+            img: 'images/cosmo.png'
+        },
+        {
+            name: 'rose',
+            img: 'images/goldrose.png'
+        },
+        {
+            name: 'lily',
+            img: 'images/lily.png'
+        },
+        {
+            name: 'mum',
+            img: 'images/mum.png'
+        },
+        {
+            name: 'windflower',
+            img: 'images/windflower.png'
         }
     ];
 
     //create game board
 
     const grid = document.querySelector('.grid')
-
-    const cardsChosen = []
-    const cardsChosenId = []
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
 
     function createBoard() {
         for (let i=0; i <= cardArray.length; i++) {
@@ -42,6 +63,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     //check for matches
+
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You Found a Match!')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Sorry, Try Again!')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congrats! You Found all of the Flowers!'
+        }
+    }
 
     //flip the card
 
